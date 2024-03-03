9ba40f93755068d08b688589e7aedf72adcd08f3
Arleny <arleny.ares@gmail.com>
2024-03-03 19:33:47 +0100

    created header, footer and main

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,11 +1,16 @@
 <script setup>
+
+import { ref, defineProps } from 'vue';
+
   const props = defineProps({
     guitarra: {
       type: Object,
       required: true
     }
   });
-        
+
+  defineEmits(['agregrar-carrito']);
+  
 </script>
 
 
@@ -17,17 +22,16 @@
               class="img-fluid"
               :src="'/img/' + guitarra.imagen + '.jpg' " 
               :alt="'imagen guitarra' + guitarra.nombre" />
-              
-            />
           </div>
           <div class="col-8">
             <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
-            <p>
-              {{ guitarra.descripcion }}
-            </p>
+            <p>{{ guitarra.descripcion }}</p>
             <p class="fw-black text-primary fs-3">{{ guitarra.precio }}</p>
-            <button type="button" class="btn btn-dark w-100">
-              Agregar al Carrito
+            <button 
+            type="button" 
+            class="btn btn-dark w-100"
+            @click="$emit('agregrar-carrito', guitarra)"
+            >Agregar al Carrito
             </button>
           </div>
       </div>
